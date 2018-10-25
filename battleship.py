7997884831a61304d84dba514dbc7eadeef2a2ea
MateMiklos <miklos.mate.91@gmail.com>
2018-10-25 16:29:08 +0200

    ready to use

--- battleship.py
+++ battleship.py
@@ -1,10 +1,12 @@
 from random import randint
 
+
 l = []
 turn = 0
 size = input("Give the size of the sea: ")
 turns = input("Give how many turns do you need: ")
 print("LET DA BATTLE BEGIN, YA LAND LUBBER!!!")
+
 
 for x in range(int(size)):
     l.append(['\x1b[0;37;44m' + 'SEA|' + '\x1b[0m'] * int(size))
@@ -15,7 +17,8 @@ for row in l:
 
 row2 = randint(0, int(size) - 1)
 col = randint(0, len(l[0]) - 1)
-print(row2, col)
+#print(row2, col)
+
 
 while turn < int(turns):
     r = int(input("ROW: "))
